3b0e29b62f57a6715e8b1f158cd2a838978aa291
Hust15900 <1205778269@qq.com>
2023-12-13 15:07:07 +0800

    demand improved

--- sensitive_word_search/Program.cs
+++ sensitive_word_search/Program.cs
@@ -11,8 +11,12 @@ namespace sensitive_word_search
             Console.InputEncoding = Encoding.UTF8;
             // 确定敏感词
             string sensitive_word = "Hello world";
-            Console.WriteLine("请输入文本");
-            string str = Console.ReadLine();
+            // 手动输入文本
+            // Console.WriteLine("请输入文本");
+            // string str = Console.ReadLine();
+            // 读取文本文件
+            StreamReader sr = new StreamReader("test.txt",Encoding.UTF8);
+            string str = sr.ReadToEnd();
             // 进行敏感词查找和替换
             if(str.Contains(sensitive_word)){
                 str = str.Replace(sensitive_word,"Hello XGogh");
